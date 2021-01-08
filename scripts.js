d13028c75e52bb33798819df0af73c1c3da7fe07
code1239 <40316183+code1239@users.noreply.github.com>
2021-01-08 12:09:55 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,58 @@
+const inputName = document.getElementById('input-name')
+const checkBtn = document.getElementById("check");
+const orderBtn = document.getElementById("order");
+const checkPopup = document.getElementById("check-popup");
+const orderPopup = document.getElementById("order-popup");
+const submitBtn = document.getElementById("submit");
+const database = firebase.database();
+
+checkBtn.addEventListener("click",  (e) => {
+  e.preventDefault()
+    const tableNo = document.getElementById("input-table").value;
+    checkPopup.classList.toggle("active") 
+
+    if (tableNo === "") console.log("please enter a number");
+    else {
+      database.ref('/customers/' + tableNo).set({
+         name: inputName.value,
+        led:  0,
+        led1:  1
+      })
+    }
+    setTimeout(() => {
+            checkPopup.classList.toggle("active")
+          }, 1300);       
+})
+orderBtn.addEventListener("click",  (e) => {
+   e.preventDefault()
+    const tableNo = document.getElementById("input-table").value;
+    orderPopup.classList.toggle("active") 
+
+    if (tableNo === "") console.log("please enter a number");
+    else {
+      database.ref('/customers/' + tableNo).set({
+         name: inputName.value,
+        led:  1,
+        led1:  0
+      })
+    }
+    setTimeout(() => {
+            orderPopup.classList.toggle("active")
+          }, 1300);       
+})
+
+submitBtn.addEventListener ("click",  (e) => {
+         e.preventDefault()
+        const tableNo = document.getElementById("input-table").value;
+        
+       database.ref('/customers/' + tableNo).set({
+         name: inputName.value,
+        led:  0,
+        led1:  0
+      })
+      
+
+       
+      })
+
+
