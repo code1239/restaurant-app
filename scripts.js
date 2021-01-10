69c624f2b10e66790e809adfdd5e804d8a879a2c
code1239 <40316183+code1239@users.noreply.github.com>
2021-01-10 16:52:20 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,55 +4,56 @@ const orderBtn = document.getElementById("order");
 const checkPopup = document.getElementById("check-popup");
 const orderPopup = document.getElementById("order-popup");
 const submitBtn = document.getElementById("submit");
-const database = firebase.database();
 
+console.log("updated 10.01.2021")
 checkBtn.addEventListener("click",  (e) => {
   e.preventDefault()
     const tableNo = document.getElementById("input-table").value;
-    checkPopup.classList.toggle("active") 
+    checkPopup.classList.toggle("active")
 
     if (tableNo === "") console.log("please enter a number");
     else {
-      database.ref('/customers/' + tableNo).set({
-         name: inputName.value,
-        led:  0,
-        led1:  1
-      })
+      // database.ref('/customers/' + tableNo).set({
+      //    name: inputName.value,
+      //   led:  0,
+      //   led1:  1
+      // })
     }
     setTimeout(() => {
             checkPopup.classList.toggle("active")
-          }, 1300);       
+          }, 1300);
 })
 orderBtn.addEventListener("click",  (e) => {
    e.preventDefault()
     const tableNo = document.getElementById("input-table").value;
-    orderPopup.classList.toggle("active") 
+    orderPopup.classList.toggle("active")
 
     if (tableNo === "") console.log("please enter a number");
     else {
-      database.ref('/customers/' + tableNo).set({
-         name: inputName.value,
-        led:  1,
-        led1:  0
-      })
+    //   database.ref('/customers/' + tableNo).set({
+    //      name: inputName.value,
+    //     led:  1,
+    //     led1:  0
+    //   })
     }
     setTimeout(() => {
             orderPopup.classList.toggle("active")
-          }, 1300);       
+          }, 1300);
 })
 
 submitBtn.addEventListener ("click",  (e) => {
          e.preventDefault()
         const tableNo = document.getElementById("input-table").value;
-        
-       database.ref('/customers/' + tableNo).set({
-         name: inputName.value,
-        led:  0,
-        led1:  0
-      })
-      
 
-       
-      })
+      //  database.ref('/customers/' + tableNo).set({
+      //    name: inputName.value,
+      //   led:  0,
+      //   led1:  0
+      // })
+
+
+
+      }
+      )
 
 
